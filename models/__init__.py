04322725d0d41eaa5a1bd7d086f9f475f2ee84a9
Viktor-3 <37882796+Viktor-3@users.noreply.github.com>
2024-10-15 09:42:13 +0200

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from models.siren import SIREN
 from models.kan import KANLinear, KAN
+from models.mfn import GaborNet
 
 
 __all__ = [
     "SIREN",
     "KANLinear",
     "KAN"
+    "GaborNet"
 ]
