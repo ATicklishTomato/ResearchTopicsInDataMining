d6eb7de020a32bc85d38757416da84ef272a790e
Luuk Wubben <luukwubben2@gmail.com>
2024-10-16 21:38:29 +0200

    Take out tensorboard and replace with wandb

--- run.py
+++ run.py
@@ -245,7 +245,6 @@ def main():
             dataloader=dataloader,
             epochs=args.epochs,
             lr=args.lr,
-            model_dir=os.path.join('./logs', args.experiment_name),
             config=config,
             device=args.device,
             log_level=args.verbose,
@@ -256,7 +255,6 @@ def main():
         test(
             model=model,
             test_dataloader=dataloader,
-            model_dir=os.path.join('./logs', args.experiment_name),
             config=config,
             device=args.device,
             log_level=args.verbose,
