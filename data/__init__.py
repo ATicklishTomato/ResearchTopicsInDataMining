b07a893ab7163ef48d6daf8a8a3b4a1e115674e8
SeppeHannen <seppe.hannen1@hotmail.com>
2024-10-25 19:45:55 +0200

    Initial commit SDF

--- data/__init__.py
+++ data/__init__.py
@@ -1,24 +1,18 @@
-from data.audio.data import AudioFile
-from data.images.data import Camera
 from data.images.summary import image_summary
-from data.sdf.summary import sdf_summary
-from data.utils import Implicit2DWrapper, ImplicitAudioWrapper, PointCloud
-from data.metrics import mean_squared_error, peak_signal_to_noise_ratio, sdf_loss, intersection_over_union, \
-    chamfer_hausdorff_distance
 from data.audio.summary import audio_summary
+from data.shapes.summary import shape_summary
+from data.utils import Implicit2DWrapper, ImplicitAudioWrapper
+from data.metrics import mean_squared_error, peak_signal_to_noise_ratio, intersection_over_union, \
+    chamfer_hausdorff_distance
 
 __all__ = [
-    "Camera",
-    "AudioFile",
-    "PointCloud",
     "Implicit2DWrapper",
     "ImplicitAudioWrapper",
     "mean_squared_error",
-    "sdf_loss",
     "peak_signal_to_noise_ratio",
     "intersection_over_union",
     "chamfer_hausdorff_distance",
     "image_summary",
     "audio_summary",
-    "sdf_summary"
+    "shape_summary"
 ]
--- data/metrics.py
+++ data/metrics.py
@@ -121,3 +121,29 @@ def chamfer_hausdorff_distance(recon_points, gt_points, eval_type="Default"):
         chamfer_dist = 0.5 * (cd_re2gt + cd_gt2re)
         hausdorff_distance = np.max((hd_re2gt, hd_gt2re))
     return chamfer_dist, hausdorff_distance, cd_re2gt, cd_gt2re, hd_re2gt, hd_gt2re
+
+def sdf(model_output, gt):
+    '''
+       x: batch of input coordinates
+       y: usually the output of the trial_soln function
+       '''
+    gt_sdf = gt['sdf']
+    gt_normals = gt['normals']
+
+    coords = model_output['model_in']
+    pred_sdf = model_output['model_out']
+
+    gradient_res = gradient(pred_sdf, coords)
+
+    # Wherever boundary_values is not equal to zero, we interpret it as a boundary constraint.
+    sdf_constraint = torch.where(gt_sdf != -1, pred_sdf, torch.zeros_like(pred_sdf))
+    inter_constraint = torch.where(gt_sdf != -1, torch.zeros_like(pred_sdf), torch.exp(-1e2 * torch.abs(pred_sdf)))
+    normal_constraint = torch.where(gt_sdf != -1, 1 - F.cosine_similarity(gradient_res, gt_normals, dim=-1)[..., None],
+                                    torch.zeros_like(gradient_res[..., :1]))
+    grad_constraint = torch.abs(gradient_res.norm(dim=-1) - 1)
+    # Exp      # Lapl
+    # -----------------
+    return {'sdf': torch.abs(sdf_constraint).mean() * 3e3,  # 1e4      # 3e3
+            'inter': inter_constraint.mean() * 1e2,  # 1e2                   # 1e3
+            'normal_constraint': normal_constraint.mean() * 1e2,  # 1e2
+            'grad_constraint': grad_constraint.mean() * 5e1}  # 1e1      # 5e1
